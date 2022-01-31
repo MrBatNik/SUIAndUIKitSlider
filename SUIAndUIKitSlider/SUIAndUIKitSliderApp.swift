2efb6094dda6494480eb6f3bb0ecb821535b9c93
Nikita <nicandreo@gmail.com>
2022-01-31 12:11:54 +0300

    Initial Commit

---
+++ SUIAndUIKitSlider/SUIAndUIKitSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIAndUIKitSliderApp.swift
+//  SUIAndUIKitSlider
+//
+//  Created by Nikita Batrakov on 31.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SUIAndUIKitSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
